730122e1f24942381ab2cb197f9cbfc128dfcff3
megmcea <mmceachran@umass.edu>
2025-06-30 16:17:10 -0500

    update code to detect overlapping events

---
+++ code/event_grid_check.R
@@ -0,0 +1,39 @@
+# This is code for checking the quality of scoring on PSGs by looking for double scored events
+# Meg McEachran
+# 6/30/2025
+
+# Load required packages
+library(dplyr)
+library(readr)
+library(janitor)
+
+# Read the CSV file
+df <- read_csv("data/original/201-010_EventGrid_60-dayPSG_05212025.csv") %>% 
+  clean_names()
+
+
+# Remove rows with missing start_epoch or end_epoch
+df <- df %>% filter(!is.na(start_epoch) & !is.na(end_epoch))
+
+# Add a row number for unique identification
+df <- df %>% mutate(row_id = row_number())
+
+#filter to just respiratory events
+df <- df %>%
+  filter(event %in% (c("A. Mixed", "A. Obstructive", "A. Central", "Hypopnea","Apnea",
+                                           "H. Obstructive", "H.Mixed")))
+# Find overlapping events
+overlaps <- expand.grid(row_id1 = df$row_id, row_id2 = df$row_id) %>%
+  filter(row_id1 < row_id2) %>%
+  left_join(df, by = c("row_id1" = "row_id")) %>%
+  left_join(df, by = c("row_id2" = "row_id"), suffix = c("_1", "_2")) %>%
+  filter(
+    (start_time_2 <= end_time_1) & (start_time_2 >= start_time_1))   %>% 
+  select(
+    event_1, start_time_1, end_time_1,
+    event_2, start_time_2, end_time_2, 
+  )
+
+overlaps
+
+#save the results to a sheet in an excel 
---
+++ code/helper_fxns.R
@@ -0,0 +1,78 @@
+#this is code that's essential for parsing large labchart exported as txt: 
+# parse_labchart_txt <- function(filepath){ 
+#   
+#   # First have to read in the file using readLines which will be able to handle windows carriage line encoding \r\n
+#   # Read all lines, removing carriage returns
+#   lines <- readLines(filepath)
+#   lines <- gsub("\r", "", lines)
+#   
+#   # Write to a temporary file
+#   temp <- tempfile()
+#   writeLines(lines, temp)
+#   
+#   # Now read the cleaned file
+#   dt <- read.table(temp,
+#                    sep = "\t",
+#                    header = F,
+#                    stringsAsFactors = FALSE,
+#                    fill = TRUE,      # Allow for ragged rows
+#                    quote = "",
+#                    comment.char = "",
+#                    strip.white = TRUE) # Optional: trim whitespace
+# 
+#   # dt<- read.table(filepath, 
+#   #                  sep = "\t", 
+#   #                  header = T,  # Set to TRUE if first row contains headers
+#   #                  stringsAsFactors = FALSE,
+#   #                  fill = F,     # Fill missing values
+#   #                  quote = "",      # No quote characters
+#   #                  comment.char = "") # No comment characters
+#   return(dt)
+# }
+
+#### okay go from here: 
+library(data.table)
+library(dplyr)
+library(tidyverse)
+library(janitor)
+# dt<-read_tsv(wfp,
+#              #col_types = cols(.default="c"),
+#              trim_ws = TRUE)
+
+  ### Read in and name the data
+parse_labchart_txt<-function(fp, downsample_rate){
+  dt<-data.table::fread(fp, strip.white = TRUE, fill = T)  #fp can be the raw labchart txt file
+  
+  # Add channel names
+  names_all<-as.character( dt[which(dt[,1]=="ChannelTitle="),])  
+  names<-unlist(str_split(names_all, pattern = "\t")) %>%
+   .[!.%in% c("ChannelTitle=","NA")] %>% 
+    make_clean_names() # %>%  
+  names<-c("sec_since_midnight","date_char",names)
+  
+  
+  # Remove the header rows from the main data
+  dt <- dt %>% slice(-c(1:(which(dt[,1]=="Range=")+1)))
+  colnames(dt)<-names 
+  
+  ### Downsampling:
+  if (missing(downsample_rate)==TRUE) {
+    return(dt)  # No downsampling needed
+  } else {
+  
+  # Downsample the data by taking every nth row
+  downsampled_dt <- dt[seq(1, nrow(dt), by = downsample_rate), ]
+  
+  ds_val<-downsample_rate * 0.005 #multiply by default labchart rate
+  return(downsampled_dt)
+  print(paste("Downsampled data by a factor of", downsample_rate, "to", ds_val, "seconds per row."))}
+}
+
+
+#function to convert seconds since midnight into an actual time: 
+ssm_to_time<-function(x){
+  h=floor(x/3600)
+  m=floor((x-(h*3600))/60)
+return(as.ITime(h))
+}
+ssm_to_time(77982)
