d92991f217ba9bdf4c9ffa56b37e38c9115ed5f6
megmcea <mmceachran@umass.edu>
2025-08-01 13:21:47 -0500

    updated odfp and helper functions sourcing, progress towards processing sleep image data

--- code/helper_fxns.R
+++ code/helper_fxns.R
@@ -1,3 +1,35 @@
+
+odfp <- function(rel_path) {
+  user<-Sys.getenv("USERNAME")
+  onedrive_path<-paste0("C:/Users/",user,"/Lunair Medical/R&D - Documents/FiH Data")
+  #if (path == "") stop("OneDrive path not found in environment variables.")
+  file.path(onedrive_path, rel_path)
+}
+
+#helper functions: 
+extrafont::loadfonts()
+
+theme_lunair <- function(textsize=24){
+  
+  theme_minimal() %+replace% 
+    theme(text = element_text(#family = "Arial", 
+      size = textsize), 
+      axis.ticks.length=unit(-0.05, "in"), 
+      axis.text.y.right = element_blank(), 
+      axis.text.x.top = element_blank(), 
+      axis.title.y.right = element_blank(),
+      axis.title.x.top = element_blank(),
+      #panel.border = element_rect(fill = NA),
+      plot.title = element_text(size = textsize,# face = "bold", 
+                                hjust = 0),
+      legend.position = "none",
+      panel.background = element_rect(fill = "white",colour = "white"),
+      plot.background = element_rect(fill = "white",colour = "white"))
+  #strip.background = element_rect(fill="white"))
+}
+
+
+
 #this is code that's essential for parsing large labchart exported as txt: 
 # parse_labchart_txt <- function(filepath){ 
 #   
